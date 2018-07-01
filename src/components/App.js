6e2e57b64b435af857881f83e2c4284347622748
zenjutahi <jeffmnjogu@gmail.com>
2018-07-01 11:36:09 +0300

    add profile and edit routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
-import Notifications from 'react-notify-toast';
-import LoginPage from './Login/LoginPage';
-import RegisterPage from './Register/RegisterPage';
-import BusinessRegisterPage from './BusinessRegister/BusinessRegisterPage';
-import BusinessListPage from './BusinessList/BusinessListPage';
+import React from "react";
+import { Route, BrowserRouter } from "react-router-dom";
+import Notifications from "react-notify-toast";
+import Homepage from "./Home/Homepage";
+import DashboardPage from "./Dashboard/DashboardPage";
+import LoginPage from "./Login/LoginPage";
+import Logout from "./Logout/Logout";
+import RegisterPage from "./Register/RegisterPage";
+import BusinessRegisterPage from "./BusinessRegister/BusinessRegisterPage";
+import BusinessListPage from "./BusinessList/BusinessListPage";
 
 const App = () => (
-    <BrowserRouter>
-        <div>
-            <Notifications />
-            <Route exact path="/" component={LoginPage} />
-            <Route exact path="/register" component={RegisterPage} />
-            <Route exact path="/businesses" component={BusinessRegisterPage} />
-            <Route exact path="/businesses/list" component={BusinessListPage} />
-        </div>
-    </BrowserRouter>
+  <BrowserRouter>
+    <div>
+      <Notifications />
+      <Route exact path="/" component={Homepage} />
+      <Route exact path="/login" component={LoginPage} />
+      <Route exact path="/logout" component={Logout} />
+      <Route exact path="/dashboard" component={DashboardPage} />
+      <Route exact path="/user/register" component={RegisterPage} />
+      <Route
+        exact
+        path="/register/businesses"
+        component={BusinessRegisterPage}
+      />
+      <Route exact path="/businesses/list" component={BusinessListPage} />
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
