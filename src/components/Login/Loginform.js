50ef538467e165c20d2f8f3622944f6490518e4e
zenjutahi <jeffmnjogu@gmail.com>
2018-07-01 11:40:33 +0300

    modify login form

--- src/components/Login/Loginform.js
+++ src/components/Login/Loginform.js
@@ -1,39 +1,59 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 class LoginForm extends React.Component {
-
-  render () {
+  render() {
     return (
       <form onSubmit={this.props.handleLogin}>
-          {this.props.message && this.props.message!=="Successfully Loged In" &&
-          <div className="alert alert-danger" role="alert">
-            <p>{this.props.message}</p>
-          </div>}
-          {!this.props.message && window.localStorage.getItem("register_message") &&
+        {this.props.message &&
+          this.props.message !== "Successfully Loged In" && (
+            <div className="alert alert-danger" role="alert">
+              <p>{this.props.message}</p>
+            </div>
+          )}
+        {!this.props.message &&
+          window.localStorage.getItem("register_message") && (
+            <div className="alert alert-success" role="alert">
+              <p>{window.localStorage.getItem("register_message")}</p>
+            </div>
+          )}
+        {this.props.message === "Successfully Loged In" && (
           <div className="alert alert-success" role="alert">
-            <p>{window.localStorage.getItem("register_message")}</p>
-          </div>}
-          {this.props.message=="Successfully Loged In" &&
-          <div className="alert alert-success" role="alert">
             <p>{this.props.message}</p>
-          </div>}
-          <div className="form-group">
-              <label className="sr-only">email</label>
-              <input type="email" name="email" id="email" className=
-              "form-control" placeholder="Email"/>
           </div>
-          <div className="form-group">
-              <label className="sr-only">password</label>
-              <input type="password" name="password" id="password" className=
-              "form-control" placeholder="Password"/>
-          </div>
-
-      <input type="submit" value="Log in" className="btn btn-primary btn-block try-back"/>
-      <br />
-      <p>Not a registred User ? Please <Link to="register" >Sign Up</Link></p>
+        )}
+        <div className="form-group">
+          <label className="sr-only">email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="form-control"
+            placeholder="Email"
+          />
+        </div>
+        <div className="form-group">
+          <label className="sr-only">password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className="form-control"
+            placeholder="Password"
+          />
+        </div>
+        <input
+          type="submit"
+          value={this.props.disabled ? "Signing in..." : "Sign in"}
+          className="btn btn-primary btn-block try-back"
+          disabled={this.props.disabled}
+        />
+        <br />
+        <p>
+          Not a registred User ? Please <Link to="/user/register">Sign Up</Link>
+        </p>
       </form>
     );
   }
-};
+}
 export default LoginForm;
