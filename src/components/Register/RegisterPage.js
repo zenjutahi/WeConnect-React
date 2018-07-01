7c741dee0e411559ff50311612804962e9718fed
zenjutahi <jeffmnjogu@gmail.com>
2018-07-01 11:42:40 +0300

    modify user resgistration page

--- src/components/Register/RegisterPage.js
+++ src/components/Register/RegisterPage.js
@@ -1,67 +1,79 @@
-import React from 'react';
-import RegisterForm from './RegisterForm';
+import React from "react";
+import RegisterForm from "./RegisterForm";
 
 class RegisterPage extends React.Component {
   state = {
     message: undefined,
-    error: undefined,
-    done: undefined
-  }
+    disabled: false
+  };
 
-  handleRegister = async (e) => {
+  handleRegister = async e => {
     e.preventDefault();
-    const first_name = e.target.first_name.value;
+    const firstName = e.target.first_name.value;
     const username = e.target.username.value;
     const email = e.target.email.value;
     const password = e.target.password.value;
 
-    const api_call = await fetch('https://weconnect-api-db.herokuapp.com/api/auth/register',{
-    method: "POST",
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      first_name: first_name,
-      username: username,
-      email: email,
-      password: password
-    })
-    });
+    this.setState({ disabled: "disabled" });
+
+    const api_call = await fetch(
+      "https://weconnect-api-db.herokuapp.com/api/auth/register",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          first_name: firstName,
+          username: username,
+          email: email,
+          password: password
+        })
+      }
+    );
     const data = await api_call.json();
-    console.log(data)
-    if (data.message == "New user Succesfully created") {
-      window.localStorage.setItem("register_message", data.message)
-      setTimeout(window.location.assign('/'), 1000);
-    };
+    if (data.message === `${username}'s account succesfully created`) {
+      window.localStorage.setItem("register_message", data.message);
+      window.location.assign("/login");
+      this.setState({
+        message: undefined
+      });
+      return;
+    }
     this.setState({
       message: data.message,
-      error: data.error
+      disabled: false
     });
+  };
 
-  }
-
-  render () {
+  render() {
     return (
       <div className="container">
-          <div className="row justify-content-center">
-              <div className="col-xs-8 col-sm-8 col-md-6 col-sm-offset-2
-              col-md-offset-4 text-center">
-                  <div className="card">
-                      <div className="card-header">
-                          <h3 className="card-title"><i className="fa fa-link"></i>Sign Up to Weconnect</h3>
-                      </div>
-
-                      <div className="card-body">
-                      <RegisterForm handleRegister={this.handleRegister} message={this.state.message}/>
-                      </div>
-                  </div>
+        <div className="row justify-content-center">
+          <div
+            className="col-xs-8 col-sm-8 col-md-6 col-sm-offset-2
+              col-md-offset-4 text-center"
+          >
+            <div className="card">
+              <div className="card-header">
+                <h3 className="card-title">
+                  <i className="fa fa-link" />Sign Up to Weconnect
+                </h3>
               </div>
-          </div>
-      </div>
 
+              <div className="card-body">
+                <RegisterForm
+                  handleSignin={this.handleRegister}
+                  message={this.state.message}
+                  disabled={this.state.disabled}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
-
-};
+}
 
 export default RegisterPage;
